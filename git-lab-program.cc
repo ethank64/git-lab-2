bd3c1fa70533ba7fcd0793b09dc9789059edd358
Ethan Knotts <ek792523@ohio.edu>
2024-09-04 17:17:42 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Ethan Knotts
+ *        Date: September 4, 2024
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
